cc27c7142617dd4a91c13604e344b14e9ff3071b
gustavojuan <gustavojuan@gmail.com>
2025-02-18 16:45:37 +0100

    feat(welcome): added StaticController for static pages

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -4,19 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WeatherController
+class WeatherController extends AbstractController
 {
+
+
     #[Route('/weather/highlander-says')]
     public function highlanderSays(): Response
     {
         $draw = random_int(0, 100);
         $forecast  = $draw < 50 ? "rain" : "sunny";
 
-        return new Response(
-            "<html><body>$forecast $draw</body></html>"
-        );
+        return $this->render('weather/hihlander_says.html.twig', compact('forecast'));
     }
 }
