2c885d8f728d8e68d756cafbf8681dcbf341ce94
gustavojuan <gustavojuan@gmail.com>
2025-02-19 16:23:42 +0100

    lesson: routing complete

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -5,13 +5,34 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+#[Route('/weather')]
 class WeatherController extends AbstractController
 {
 
-    // #[Route('/weather/highlander-says/{treshold<\d+>?50}')]
+    #[Route('/highlander-says/{treshold<\d+>?50}', host: 'api.localhost')]
+    public function highlanderSaysApi(int $treshold): JsonResponse
+    {
+        $draw = random_int(0, 100);
+        $forecast  = $draw < $treshold ? "rain" : "sunny";
+
+        $json  = [
+            'forecast' => $forecast,
+            'treshold' => $treshold,
+            'self' => $this->generateUrl(
+                'app_weather_highlandersaysapi',
+                compact('treshold'),
+                UrlGeneratorInterface::ABSOLUTE_URL
+            )
+        ];
+        return new JsonResponse($json);
+    }
+
+    // #[Route('/highlander-says/{treshold<\d+>?50}')]
     public function highlanderSays(int $treshold): Response
     {
         $draw = random_int(0, 100);
@@ -20,10 +41,17 @@ class WeatherController extends AbstractController
         return $this->render('weather/highlander_says.html.twig', compact('forecast'));
     }
 
-    //#[Route('/weather/highlander-says/{guess}')]
+    // #[Route('/highlander-says/{guess}')]
     public function highlanderSaysGuess(string $guess): Response
     {
         $forecast  = "It's going to $guess";
+        return $this->render('weather/highlander_says.html.twig', compact('forecast'));
+    }
+
+    // #[Route('/test', name: 'customtest')]
+    public function test(): Response
+    {
+        $forecast  = "TEST";
         return $this->render('weather/highlander_says.html.twig', compact('forecast'));
     }
 }
