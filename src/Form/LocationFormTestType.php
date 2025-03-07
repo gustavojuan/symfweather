b1938fd29a9a363ab1ac4e147000f3bb452e3327
gustavojuan <gustavojuan@gmail.com>
2025-03-07 13:13:08 +0100

    lesson: Forms

---
+++ src/Form/LocationFormTestType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationFormTestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('countryCode', ChoiceType::class, [
+                'choices' => [
+                    '' => null,
+                    'Spain' => 'ES',
+                    'Poland' => 'PL',
+                    'United kingdom' => 'UK'
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+                'attr' => [
+                    'step' => 0.1,
+                    'min' => -90,
+                    'max' => 90,
+                ]
+            ])
+            ->add('longitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+                'attr' => [
+                    'step' => 0.1,
+                    'min' => -90,
+                    'max' => 90,
+                ]
+            ])
+            ->add('submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+
+}
